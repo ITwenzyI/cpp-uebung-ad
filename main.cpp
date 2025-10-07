df458baa4a7c8cb14fb41e5022f98e82901c5dee
Kilian <kischoenhaar@gmail.com>
2025-10-07 15:40:39 +0200

    inital cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main
+ * Author: Kilian | ItwenzyI
+ * Created: 07.10.2025
+ * Description: 
+ */
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
