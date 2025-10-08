4b1f0629c160b8899c7a226065c2352ec14fb6f7
Kilian <kischoenhaar@gmail.com>
2025-10-08 11:13:54 +0200

    add mult and add for matrizen

--- uebung01/03.cpp
+++ uebung01/03.cpp
@@ -47,14 +47,59 @@ private:
     }
 
 
+    Matrix add(const Matrix& x) {
+        Matrix y = Matrix(x.zeilen, x.spalten);
+
+        if (this->zeilen == x.zeilen && this->spalten == x.spalten) {
+            for (int i = 0; i < zeilen; i++) {
+                for (int j = 0; j < spalten; j++) {
+                    y.data[i*spalten + j] = data[i*spalten + j] + x.data[i*zeilen + j];
+                }
+            }
+        }
+        else {
+            std::cout << "Fehler, nicht gleiche Spalten und Zeilen Anzahl der Matrizen!" << std::endl;
+        }
+        return y;
+    }
+
+    Matrix mult(const Matrix& x) {
+        Matrix y(this->zeilen, x.spalten);
+
+        if (this->spalten == x.zeilen) {
+            for (int i = 0; i < zeilen; i++) {
+                for (int j = 0; j < x.spalten; j++) {
+                    int sum = 0;
+
+                    for (int k = 0; k < this->spalten; k++) {
+                        sum += data[i * this->spalten + k] * x.data[k * x.spalten + j];
+                    }
+                    y.data[i * x.spalten + j] = sum;
+                }
+            }
+        }
+        else {
+            std::cout << "Fehler, die Matrize 1 muss gleich viele Spalten wie die Matrize 2 Zeilen haben!" << std::endl;
+        }
+
+        return y;
+    }
+
+
+
 
 
 };
 
 int main() {
-    Matrix a(2, 3);
-    a.input();
+    Matrix a(2, 2);
+    a.input(); // (1, 2 | 3, 4)
     a.print();
+    Matrix b(2, 3);
+    b.input(); // (1, 2, 3 | 4, 5, 6)
+    b.print();
+    Matrix c = a.mult(b);
+    c.print();
 
     return 0;
 }
